def29d498a75552a26bea535939bdde6eb38a543
MohamedElshorbgy <167981209+MohamedElshorbgy@users.noreply.github.com>
2026-06-24 15:19:28 +0200

    Update Rounding.c

--- OnDeviceTraining/src/arithmetic/Rounding.c
+++ OnDeviceTraining/src/arithmetic/Rounding.c
@@ -7,10 +7,12 @@
 #include "Rounding.h"
 
 // C round(): rounds half away from zero (C17 7.12.9.6)
-int32_t roundHalfAway(float input) {
+int32_t roundHalfAway(float input) {                                                  //It rounds the number to the nearest integer. If the fractional part is exactly \(0.5\),
+                                                                                        it rounds away from zero (towards positive infinity for positive numbers, and towards negative infinity for negative numbers).
     return round(input);
 }
-
+                                                                                      //Based on standard implementations of stochastic rounding, this function is assumed to return a random uniform floating-point 
+                                                                                        value in the range \([0.0, 1.0)\).
 float randfloat() {
     return rngNextFloat();
 }
