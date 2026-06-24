b9d4b24651c5e53871eeabf690661c170300b4ac
MohamedElshorbgy <167981209+MohamedElshorbgy@users.noreply.github.com>
2026-06-24 15:04:16 +0200

    Update Common.h

--- OnDeviceTraining/src/common/include/Common.h
+++ OnDeviceTraining/src/common/include/Common.h
@@ -1,6 +1,6 @@
-#ifndef ODT_COMMON_H                                                                                     ////mean that if the file ODT_COMMON_H not defined , define it and read the file otherwise skip the file
+#ifndef ODT_COMMON_H                                                                                     //mean that if the file ODT_COMMON_H not defined , define it and read the file otherwise skip the file
 #define ODT_COMMON_H
-
+                                                                                                        
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -19,7 +19,11 @@
 #define DLEVEL 0
 #endif
 
-#define PRINT_DEBUG(str, ...)                                                                      \
+#define PRINT_DEBUG(str, ...)                                                                      \        //Logging Macros: Logging means writing down notes while the program runs
+                                                                                               
+                                                                                                          ##__VA_ARGS__ is a special trick used in macros to handle a variable number of arguments (inputs)                                                                                                          #define PRINT_INFO(str, ...)  printf(str, ##__VA_ARGS__):This tells the compiler,
+                                                                                                          "Hey, after the first text string (str),                                                                                                         the user might type a bunch of other variables. Just grab them all."
+                                                                                                          __VA_ARGS__: This is a placeholder. The compiler will take whatever extra variables it grabbed in the ...                                                                                                           and paste them right here
     do {                                                                                           \
         if (DLEVEL >= 3) {                                                                         \
             printf("\033[0;33m[%s: %s] ", SOURCE_FILE, __FUNCTION__);                              \
